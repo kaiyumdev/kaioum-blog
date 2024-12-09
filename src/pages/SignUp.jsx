d49d5ece5dcd4d19607c391192863d72bb6e686c
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-09 21:11:34 +0600

    remove some errors file

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,7 +2,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
-import OAuth from '../components/OAuth';
 const SignUp = () => {
     const [formData, setFormData] = useState({});
     const [errorMessage, setErrorMessage] = useState(null);
@@ -102,7 +101,6 @@ const SignUp = () => {
                                 'Sign Up'
                             )}
                         </Button>
-                        <OAuth></OAuth>
                     </form>
                     <div className='flex gap-2 text-sm mt-5'>
                         <span>Have an account?</span>
