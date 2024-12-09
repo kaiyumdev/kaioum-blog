eea1df0dbf2aa2f59b0347bd4c2aadcad192ddaa
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-09 19:53:17 +0600

    create oAuth page for google authentication

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,12 +1,10 @@
 import { Button } from "flowbite-react"
-import { AiFillGoogleCircle } from "react-icons/ai"
+import { AiFillGoogleCircle } from "react-icons/ai";
 
 const OAuth = () => {
-    const handleGoogleClick = () => {
 
-    }
     return (
-        <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
+        <Button type='button' gradientDuoTone='pinkToOrange' outline>
             <AiFillGoogleCircle className='w-6 h-6 mr-2' />
             Continue with Google
         </Button>
