15ecc29358d276372ade42108bad1cf7cf62419d
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-09 17:48:32 +0600

    create firebase file & setup firebase env successfully

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -2,6 +2,9 @@ import { Button } from "flowbite-react"
 import { AiFillGoogleCircle } from "react-icons/ai"
 
 const OAuth = () => {
+    const handleGoogleClick = () => {
+
+    }
     return (
         <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
             <AiFillGoogleCircle className='w-6 h-6 mr-2' />
