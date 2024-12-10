ed4e78243beb94001dc0271071059659b033e497
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-10 17:05:24 +0600

    pass google signin infoormation at server by api route

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -2,15 +2,33 @@ import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from '../firebase';
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const OAuth = () => {
     const auth = getAuth(app)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const handleGoogleClick = async () => {
         const provider = new GoogleAuthProvider()
         provider.setCustomParameters({ prompt: 'select_account' })
         try {
             const resultsFromGoogle = await signInWithPopup(auth, provider)
-            console.log(resultsFromGoogle)
+            const res = await fetch('/api/auth/google', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    name: resultsFromGoogle.user.displayName,
+                    gmail: resultsFromGoogle.user.email,
+                    googlePhotoUrl: resultsFromGoogle.user.photoUrl
+                })
+            })
+            const data = await res.json()
+            if (res.ok) {
+                dispatch(signInSuccess(data))
+                navigate('/')
+            }
         } catch (error) {
             console.log(error);
         }
