48b66e9c0366136b3e1abaa3656d5961473bab16
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-14 12:01:43 +0600

    dashboard tab url

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
 
 const Dashboard = () => {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+        // if (tabFromUrl) {
+        //     setTab(tabFromUrl);
+        // }
+        console.log(tabFromUrl)
+    }, [location.search]);
     return (
         <div>Dashboard</div>
     )
