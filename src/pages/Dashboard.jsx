7a19a6dea04ec0d656957786502f9a4711b2ef57
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-14 12:21:03 +0600

    add dashSidebar & dashProfile at dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 const Dashboard = () => {
     const location = useLocation();
@@ -7,13 +9,28 @@ const Dashboard = () => {
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
         const tabFromUrl = urlParams.get('tab');
-        // if (tabFromUrl) {
-        //     setTab(tabFromUrl);
-        // }
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
         console.log(tabFromUrl)
     }, [location.search]);
     return (
-        <div>Dashboard</div>
+        <div className='min-h-screen flex flex-col md:flex-row'>
+            <div className='md:w-56'>
+                {/* Sidebar */}
+                <DashSidebar />
+            </div>
+            {/* profile... */}
+            {tab === 'profile' && <DashProfile />}
+            {/* posts... */}
+            {/* {tab === 'posts' && <DashPosts />} */}
+            {/* users */}
+            {/* {tab === 'users' && <DashUsers />} */}
+            {/* comments  */}
+            {/* {tab === 'comments' && <DashComments />} */}
+            {/* dashboard comp */}
+            {/* {tab === 'dash' && <DashboardComp />} */}
+        </div>
     )
 }
 
