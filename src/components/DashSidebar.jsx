bc8c42cd9c074263906347563e4274cc581908ac
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-14 14:01:03 +0600

    updated dashSidebar with path url

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,17 +1,31 @@
 import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from "react";
 import {
     HiUser,
     HiArrowSmRight,
 } from 'react-icons/hi';
+import { Link, useLocation } from "react-router-dom";
 
 const DashSidebar = () => {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+        console.log(tabFromUrl)
+    }, [location.search]);
     return (
-        <Sidebar>
+        <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Sidebar.Item active icon={HiUser} label={'User'} labelColor="dark">
-                        Profile
-                    </Sidebar.Item>
+                    <Link to="dashboard?tab=profile">
+                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor="dark">
+                            Profile
+                        </Sidebar.Item>
+                    </Link>
                     <Sidebar.Item active icon={HiArrowSmRight} className="cursor-pointer">
                         Sign Out
                     </Sidebar.Item>
