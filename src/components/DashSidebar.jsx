577b05c5abf1823b71c2f5b268405fdc6d3fdc97
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-14 12:46:35 +0600

    updated DashSidebar

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,7 +1,23 @@
+import { Sidebar } from "flowbite-react";
+import {
+    HiUser,
+    HiArrowSmRight,
+} from 'react-icons/hi';
 
 const DashSidebar = () => {
     return (
-        <div>DashSidebar</div>
+        <Sidebar>
+            <Sidebar.Items>
+                <Sidebar.Group>
+                    <Sidebar.Item active icon={HiUser} label={'User'} labelColor="dark">
+                        Profile
+                    </Sidebar.Item>
+                    <Sidebar.Item active icon={HiArrowSmRight} labelColor="dark">
+                        Sign Out
+                    </Sidebar.Item>
+                </Sidebar.Group>
+            </Sidebar.Items>
+        </Sidebar>
     )
 }
 
